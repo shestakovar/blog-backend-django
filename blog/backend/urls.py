149b66607100c4858efed8739adc6cac411c9bf6
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-08-29 19:27:50 +0400

    Add Post and Comments views and serializers

--- blog/backend/urls.py
+++ blog/backend/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from .views import PostAPIView, CommentAPIView
+
 router = DefaultRouter()
+router.register(r'posts', PostAPIView)
+router.register(r'comments', CommentAPIView)
 
 urlpatterns = [
     path('', include(router.urls)),
--- blog/backend/views.py
+++ blog/backend/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+from rest_framework import viewsets
+
+
+class PostAPIView(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentAPIView(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
