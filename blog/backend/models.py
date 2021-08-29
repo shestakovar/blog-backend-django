4222e706adff241aa9ae075b1aaa4d95b153be7f
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-08-29 18:57:46 +0400

    Add post and comment models

--- blog/backend/models.py
+++ blog/backend/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return "%s %s" % (self.author, self.title)
+
+
+class Comment(models.Model):
+    origin = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return "%s %s %s" % (self.origin, self.author, self.content)
