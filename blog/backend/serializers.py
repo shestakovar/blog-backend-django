872c89327d5afad72686921581e44e63e1e9b2f6
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-08-31 01:40:57 +0400

    Fix POST posts and comments

--- blog/backend/serializers.py
+++ blog/backend/serializers.py
@@ -4,23 +4,19 @@ from django.contrib.auth.models import User
 from .models import Post, Comment
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', )
-
-
 class PostSerializer(serializers.ModelSerializer):
-    author = UserSerializer()
+    author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
         model = Post
         fields = '__all__'
+        read_only_fields = ('origin', )
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = UserSerializer()
+    author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
         model = Comment
         fields = '__all__'
+        read_only_fields = ('origin', )
--- blog/backend/views.py
+++ blog/backend/views.py
@@ -2,18 +2,31 @@ from django.shortcuts import render
 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
 
 from rest_framework import viewsets
+from rest_framework import permissions
 
 
 class PostAPIView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class CommentAPIView(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
 
     def get_queryset(self):
         return Comment.objects.filter(origin=self.kwargs['post_pk'])
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user,
+                        origin=Post.objects.get(pk=self.kwargs['post_pk']))
