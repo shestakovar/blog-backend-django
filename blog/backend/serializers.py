49cff39714f002495beaff24aae22a34b9794a6e
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-08-30 22:22:45 +0400

    Add username to posts and comments response

--- blog/backend/serializers.py
+++ blog/backend/serializers.py
@@ -1,15 +1,27 @@
+from django.db.models import fields
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from .models import Post, Comment
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', )
+
+
 class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+
     class Meta:
         model = Post
         fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+
     class Meta:
         model = Comment
         fields = '__all__'
