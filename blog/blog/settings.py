32efcc6d3ae5d8a3ba983176a8fe19f30ddf2b5a
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-09-05 00:05:19 +0300

    Add JWT auth

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -133,4 +133,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d.%m.%Y %H:%M',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
