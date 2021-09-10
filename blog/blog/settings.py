44eca7318409a6ae49ce424bdf4f527a064b7b0b
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-09-10 14:26:02 +0300

    Return back session auth to log in on backend DRF api

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -140,6 +140,7 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d.%m.%Y %H:%M',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
